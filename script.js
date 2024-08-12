6b307dfeb8d97c8e9970838587ea85378d2591fa
pryntl <pryntl2007@gmail.com>
2024-08-12 22:11:35 +0330

    update [java script] : comment
    added comments to explain the code.

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
+//function structure.
 function stars(number) {
+    //until 'i' is less than the number, it keeps getting more.
   for (let i = 1; i <= number; i++) {
+    //the star will be printed, 'i' times.
     console.log("*".repeat(i));
   }
+  //then when 'i' is one less than the number, it keeps getting less, until it reaches 1.
   for (let i = number - 1; i >= 1; i--) {
+    //the star will be printed, 'i' times.
     console.log("*".repeat(i));
   }
 }
+//testing.
 stars(3);
